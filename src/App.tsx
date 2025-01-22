0757e2d4a5a21d72fa76bccd21f2df12450afff6
Matt Rintoul <matt@sayyeah.com>
2025-01-22 14:11:49 -0500

    Add user info header, remove unused components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import Home from "./views/Home";
 import { SequenceKit } from "@0xsequence/kit";
 import { config } from "./config";
 import "@0xsequence/design-system/styles.css";
-import "boilerplate-design-system/styles.css";
 
-const App = () => {
+import { useAccount, useDisconnect, useSwitchChain } from "wagmi";
+import { NotConnected } from "./views/NotConnected";
+import { Connected } from "./views/Connected";
+import { SequenceBoilerplate } from "boilerplate-design-system";
+
+export default function Layout() {
   return (
     <SequenceKit config={config}>
-      <Home />
+      <App />
     </SequenceKit>
   );
-};
+}
 
-export default App;
+function App() {
+  const { isConnected } = useAccount();
+  return (
+    <SequenceBoilerplate
+      githubUrl="https://github.com/0xsequence-demos/kit-embedded-wallet-react-boilerplate"
+      name="Sequence Kit Starter - React"
+      description="Embedded Wallet"
+      wagmi={{ useAccount, useDisconnect, useSwitchChain }}
+    >
+      {isConnected ? <Connected /> : <NotConnected />}
+    </SequenceBoilerplate>
+  );
+}
