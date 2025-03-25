921a34f6dea52b0902419262b21950f372601fc9
Tomasz Dysinski <dysinski.tomasz@gmail.com>
2025-03-25 10:21:36 -0700

    chains are now configurable via env vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,6 +5,9 @@ interface ImportMetaEnv {
   readonly VITE_WAAS_CONFIG_KEY: string;
   readonly VITE_GOOGLE_CLIENT_ID: string;
   readonly VITE_APPLE_CLIENT_ID: string;
+  readonly VITE_WALLET_CONNECT_ID: string;
+  readonly VITE_CHAINS: string;
+  readonly VITE_DEFAULT_CHAIN: string;
 }
 
 interface ImportMeta {
